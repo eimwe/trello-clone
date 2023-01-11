25fce3bccfe64a3567ace7b3873871558f72a228
eimwe <prokopenkoserge@inbox.ru>
2023-01-11 14:59:29 +0600

    Completed Authorization modal

--- src/components/DialogModalAuth.tsx
+++ src/components/DialogModalAuth.tsx
@@ -1,26 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 import DialogModal from "./DialogModal";
+import ButtonTemplate from "./Button";
+
+const SubmitButton = styled(ButtonTemplate)`
+  display: block;
+  min-width: 25%;
+  min-height: 25px;
+  margin-inline: auto;
+  border-radius: 4px;
+  text-transform: uppercase;
+  background-color: rgb(143, 208, 207);
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(128, 187, 186);
+  }
+`;
+
+const TextInput = styled.input`
+  display: block;
+  min-width: 50%;
+  min-height: 25px;
+  padding-block: 0;
+  margin-inline: auto;
+  margin-block-end: 1em;
+  border-radius: 4px;
+`;
 
 const DialogModalAuth = () => {
   const [isOpened, setIsOpened] = useState(false);
+  const [userName, setUserName] = useState (() => {
+    const savedItem = localStorage.getItem("userName");
+    const parsedItem = JSON.parse(savedItem !);
+    return parsedItem || "";
+  });
 
-  const onProceed = () => {
-    console.log("Proceed clicked");
-  };
+  useEffect(() => {
+    setIsOpened(true);
+    if (userName.length > 0) {
+      localStorage.setItem("Username", JSON.stringify(userName));
+    }
+  }, [userName])
 
   return (
-    <div>
-      <button onClick={() => setIsOpened(true)}>Open "dialog" modal</button>
-
-      <DialogModal
-        title="What's your name?"
-        isOpened={isOpened}
-        onProceed={onProceed}
-        onClose={() => setIsOpened(false)}
+    <DialogModal
+    title="What's your name?"
+    isOpened={isOpened}
+    onClose={() => setIsOpened(false)}
+    >
+      <form
+        onSubmit = {(e) => e.preventDefault()}
       >
-        <p>To close: click Close, press Escape, or click outside.</p>
-      </DialogModal>
-    </div>
+        <TextInput
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <SubmitButton
+          type="submit"
+          title="ok"
+          onClick={() => setIsOpened(false)}
+        />
+      </form>
+    </DialogModal>
   );
 };
 
